43e968a0b3e6f75f444009c1d99fd710c22d2268
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-20 10:13:37 +1200

    Create ProductCollectionsDAOTest.java

---
+++ src/test/java/dao/ProductCollectionsDAOTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package dao;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Hugo
+ */
+public class ProductCollectionsDAOTest {
+    
+    public ProductCollectionsDAOTest() {
+    }
+
+    @Test
+    public void testSaveProduct() {
+    }
+
+    @Test
+    public void testRemoveProduct() {
+    }
+
+    @Test
+    public void testGetProducts() {
+    }
+
+    @Test
+    public void testGetCategories() {
+    }
+
+    @Test
+    public void testSearchById() {
+    }
+
+    @Test
+    public void testFilterByCategory() {
+    }
+    
+}
