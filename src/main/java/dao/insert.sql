896fc9a250f4134213cb2759cbc6764c9f05bedd
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-21 12:58:28 +1200

    sql files

---
+++ src/main/java/dao/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO PRODUCT
+VALUES(1, 'Dalbelo Boots', 'Green boots', 'Boots', 800, 12);
+
+INSERT INTO PRODUCT VALUES(
+    2, 'Solomon Poles', 'Black poles', 'Poles', 300, 9
+);
+
+INSERT INTO PRODUCT VALUES(
+    3, 'Burton Jacket', 'Orange jacket', 'Outer Wear', 500, 10
+);
+
+INSERT INTO PRODUCT VALUES(
+    4, 'Volcolm Pants', 'Black pants', 'Outer Wear', 450, 15
+);
+
+INSERT INTO PRODUCT VALUES(
+    5, 'Smith Goggles', 'Red goggles', 'Goggles', 200, 5
+);
+
+INSERT INTO PRODUCT VALUES(
+    6, 'Anon Goggles', 'Black goggles', 'Goggles', 150, 7
+);
+
+INSERT INTO PRODUCT VALUES(
+    7, 'Spyder Goggles', 'Pink goggles', 'Goggles', 220, 3
+);
+
+INSERT INTO PRODUCT VALUES(
+    8, 'Mons Royale Socks', 'Grey and Blue socks', 'Accessorie', 70, 14
+);
+
+INSERT INTO PRODUCT VALUES(
+    9, 'IceBreaker Beanie', 'White Striped beanie', 'Accessories', 50, 20
+);
+
+INSERT INTO PRODUCT VALUES(
+    10, 'Volkl ski Boots', 'Purple ski boots', 'BOOTS', 90, 8
+);
+
