d3e791ca965aad227e74c357a3b0183142ca11f2
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-09-01 16:42:28 +1200

    adding exception handling to creating an account

--- src/main/java/servlets/CreateAccountServlet.java
+++ src/main/java/servlets/CreateAccountServlet.java
@@ -5,19 +5,25 @@
 package servlets;
 
 import dao.CustomerDAO;
+import dao.DaoExceptionMapper;
+import dao.DaoFactory;
 import dao.JdbiDaoFactory;
 import domain.Customer;
 import java.io.IOException;
+import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import net.sf.oval.ConstraintViolation;
+import net.sf.oval.Validator;
+import net.sf.oval.exception.ConstraintsViolatedException;
 
 /**
  * Servlet for creating an account
- * 
+ *
  * @author Hugo
  */
 @WebServlet(name = "CreateAccountServlet", urlPatterns = {"/create-account"})
@@ -36,21 +42,47 @@ public class CreateAccountServlet extends HttpServlet {
             throws ServletException, IOException {
         // TODO what is the difference between response and request?
         // TODO does request care if the inputs are inside divs?
-        String userName = request.getParameter("username");
+        String username = request.getParameter("username");
         String firstName = request.getParameter("firstname");
         String lastName = request.getParameter("lastname");
         String email = request.getParameter("email");
         String address = request.getParameter("address");
         String password = request.getParameter("password");
-        
-        // CustomerDAO dao = new CustomerCollectionsDAO();
-        CustomerDAO dao = JdbiDaoFactory.getCustomerDAO();
-        Customer customer = new Customer(userName, firstName, lastName, address, email);
-        customer.setPassword(password);
-        
-        dao.saveCustomer(customer);
-       
-        response.sendRedirect("index.jsp");
+
+        CustomerDAO dao = DaoFactory.getCustomerDAO();
+        HttpSession session = request.getSession();
+        if (dao.searchByUsername(username) != null) {
+            session.setAttribute("validation", "Account with username already exists!");
+            response.sendRedirect("create-account.jsp");
+        } else {
+            Customer customer = new Customer(username, firstName, lastName, address, email);
+            session.setAttribute("validation", "");
+            customer.setPassword(password);
+            try {
+                new Validator().assertValid(customer);
+                dao.saveCustomer(customer);
+            } catch (ConstraintsViolatedException cve) {
+                // Server side checking if for some reason html form requirements fail
+                ConstraintViolation[] violations = cve.getConstraintViolations();
+                String msg = "Please fix the following input problems:";
+
+                msg += "<ul>";
+                for (ConstraintViolation cv : violations) {
+                    msg += "<li>" + cv.getMessage() + "</li>";
+                }
+                msg += "</ul>";
+
+                request.getSession().setAttribute("validation", msg);
+                response.sendRedirect("create-account.jsp");
+            } catch (Exception ex) {
+                String msg = new DaoExceptionMapper().messageFromException(ex);
+                request.getSession().setAttribute("validation", msg);
+            } finally {
+                response.sendRedirect("index.jsp");
+            }
+        }
     }
     
+    
+
 }
